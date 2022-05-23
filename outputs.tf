aea41773b1172f550962190771397705b30a3eea
Jamie Taffurelli <jltaffurelli@outlook.com>
2022-05-24 00:41:11 +0100

    added outputs

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,9 @@
+output "sql_backup_resource_id" {
+  value       = jsondecode(azurerm_resource_group_template_deployment.vm.output_content).resourceID.value
+  description = "Resource ID of the sql backup association"
+}
 
+output "image_template_resource_id" {
+  value       = jsondecode(azurerm_resource_group_template_deployment.autobackupsqlvm.output_content).resourceID.value
+  description = "Resource ID of the auto sql backup association"
+}
